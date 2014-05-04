7ce99fd4b5f08b98d11746c1f6549c93f1541f4f
Charles Feduke <charles.feduke@gmail.com>
2014-05-03 21:52:44 -0400

    finger tracking

--- SpaceRun/DTZMyScene.m
+++ SpaceRun/DTZMyScene.m
@@ -1,5 +1,9 @@
 #import "DTZMyScene.h"
 
+@interface DTZMyScene ()
+@property (nonatomic, weak) UITouch *shipTouch;
+@end
+
 @implementation DTZMyScene
 
 - (id)initWithSize:(CGSize)size
@@ -11,9 +15,22 @@
         SKSpriteNode *ship = [SKSpriteNode spriteNodeWithImageNamed:name];
         ship.position = CGPointMake(size.width / 2, size.height / 2);
         ship.size = CGSizeMake(40, 40);
+        ship.name = @"ship";
         [self addChild:ship];
     }
     return self;
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.shipTouch = [touches anyObject];
+}
+
+- (void)update:(NSTimeInterval)currentTime
+{
+    if (self.shipTouch) {
+        SKNode *ship = [self childNodeWithName:@"ship"];
+        ship.position = [self.shipTouch locationInNode:self];
+    }
+}
 @end
