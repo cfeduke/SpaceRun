c1fca62c221d3e9269a04421c333bd6fb0eb4f81
Charles Feduke <charles.feduke@gmail.com>
2014-05-03 21:40:20 -0400

    replaced MyScene

--- SpaceRun/DTZMyScene.m
+++ SpaceRun/DTZMyScene.m
@@ -1,53 +1,18 @@
-//
-//  DTZMyScene.m
-//  SpaceRun
-//
-//  Created by Charles Feduke on 5/3/14.
-//  Copyright (c) 2014 Deployment Zone. All rights reserved.
-//
-
 #import "DTZMyScene.h"
 
 @implementation DTZMyScene
 
--(id)initWithSize:(CGSize)size {    
+- (id)initWithSize:(CGSize)size
+{
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
+        self.backgroundColor = [SKColor blackColor];
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        NSString *name = @"Spaceship.png";
+        SKSpriteNode *ship = [SKSpriteNode spriteNodeWithImageNamed:name];
+        ship.position = CGPointMake(size.width / 2, size.height / 2);
+        [self addChild:ship];
     }
     return self;
-}
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
-
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
 }
 
 @end
